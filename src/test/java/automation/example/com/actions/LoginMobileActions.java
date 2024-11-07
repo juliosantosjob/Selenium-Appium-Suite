ae85bb09a57acb9351d2d247c0feb547361587a5
Julio Santos <julio958214@gmail.com>
2024-11-06 22:13:33 -0300

    Atualizar o LoginMobileActions.java

--- src/test/java/automation/example/com/actions/LoginMobileActions.java
+++ src/test/java/automation/example/com/actions/LoginMobileActions.java
@@ -7,18 +7,23 @@ public class LoginMobileActions {
 
     public void openApp() {
         visible(appLogo);
+        System.out.println("Abre o aplicativo e verifica a visibilidade do logo.");
     }
 
     public void fillForm(String username, String password) {
         type(usernameField, username);
         type(passwordField, password);
+        System.out.println("Preenche o formulário com o usuário: " + username + " e senha.");
     }
 
     public void submitLogin() {
         click(loginButton);
+        System.out.println("Submete o login.");
     }
 
     public String getTextProduct() {
-        return grabText(textProduct);
+        String productText = grabText(textProduct);
+        System.out.println("Obtém o texto do produto: " + productText);
+        return productText;
     }
 }
